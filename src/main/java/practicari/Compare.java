8ca97934f86496cf738ea26c6c2ff3111fa4ac03
Lucia <l.alvarezg@udc.es>
2024-04-29 13:05:35 +0200

    implementada clase Compare

--- src/main/java/practicari/Compare.java
+++ src/main/java/practicari/Compare.java
@@ -1,4 +1,13 @@
 package practicari;
+
+import org.apache.commons.math3.exception.*;
+import org.apache.commons.math3.stat.inference.TTest;
+import org.apache.commons.math3.stat.inference.WilcoxonSignedRankTest;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Compare {
     private static final String usage = "Uso: -test t | wilcoxon <alpha> -results <results1.csv> <results2.csv>\n" +
             "  -test t | wilcoxon <alpha>: test de significancia estadística (t-test o Wilcoxon) y nivel de " +
@@ -7,8 +16,8 @@ public class Compare {
             "obtenidos con TrainingTestTrecCovid para la misma métrica y sobre las mismas queries de " +
             "test.\n";
     public static void main(String[] args) {
-        String test = null;
-        String signif = null;
+        String testOption = null;
+        String alphaString = null;
         double alpha;
         String results1 = null, results2 = null;
 
@@ -16,8 +25,8 @@ public class Compare {
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-test":
-                    test = args[++i];
-                    signif = args[++i];
+                    testOption = args[++i];
+                    alphaString = args[++i];
                     break;
                 case "-results":
                     results1 = args[++i];
@@ -26,11 +35,11 @@ public class Compare {
                 default:
                     System.out.println("Parámetro desconocido: " + args[i]);
                     System.out.println(usage);
-                    return;
+                    System.exit(1);
             }
         }
 
-        if(test == null || signif == null) {
+        if(testOption == null || alphaString == null) {
             System.out.println("El argumento \"test\" y sus parámetros son obligatorios.");
             System.exit(-1);
         } else if(results1 == null || results2 == null) {
@@ -38,21 +47,81 @@ public class Compare {
             System.exit(-1);
         }
 
-        // parsear signif en alpha
-        alpha = Double.parseDouble(signif);
+        // parsear alpha
+        alpha = tryParseDouble(alphaString, "Parámetro alpha no es un número válido.");
+        double[] data1 = getData(results1);
+        double[] data2 = getData(results2);
 
 
+        if(testOption.equals("t")) {
+            TTest test = new TTest();
+            double pValue = test.pairedTTest(data1, data2);
+            printVeredict(pValue, alpha);
+        }
+        else if(testOption.equals("wilcoxon")) {
+            WilcoxonSignedRankTest test = new WilcoxonSignedRankTest();
+            try {
+                double pValue = test.wilcoxonSignedRankTest(data1, data2, false);
+                printVeredict(pValue, alpha);
+            }  catch (NullArgumentException | NoDataException e) {
+                System.err.println("No hay datos en los archivos de resultados: " + e.getMessage());
+                System.exit(1);
+            } catch (ConvergenceException e) {
+                System.err.println("No se puede calcular el p-value por un error de convergencia: " + e.getMessage());
+                System.exit(1);
+            } catch (MaxCountExceededException e) {
+                System.err.println("Número máximo de iteraciones sobrepasado en el test: " + e.getMessage());
+                System.exit(1);
+            }
+
+
+        }
 
     }
 
-    public static Double parseDouble(String signif) {
-        try {
-            return Double.parseDouble(signif);
-        } catch (NumberFormatException e) {
-            System.err.println("Error al parsear alpha \"" + signif + "\": " + e.getMessage());
-            System.exit(-1);
-        }
-        return 0.0;
+    private static void printVeredict(double pValue, double alpha) {
+        System.out.println("P-value: " + pValue);
+        if(pValue < alpha)
+            System.out.println("Como el p-value es menor, la hipótesis nula puede ser rechazada.");
+        else
+            System.out.println("Como el p-value es mayor, la hipótesis nula no se rechaza.");
+    }
 
+    private static double[] getData(String path) {
+        File file = new File(path);
+        List<Double> lista = new LinkedList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = reader.readLine();      // ignoramos la línea de cabecera
+            while ((line = reader.readLine()) != null) {
+                String[] tmp = line.split(",");
+                lista.add(tryParseDouble(tmp[1], "Los datos del archivo " + path + " no son válidos."));
+            }
+
+            // meter la lista en un double[]
+            double[] acc = new double[lista.size()];
+            int i = 0;
+            for (double elem : lista) {
+                acc[i] = elem;
+                i++;
+            }
+            return acc;
+        } catch (IOException e) {
+            System.err.println("Error de E/S al leer el archivo " + path + ": " + e.getMessage());
+            System.exit(1);
+        }
+
+
+        return null;
+    }
+
+    private static double tryParseDouble(String n, String errMsg) {
+        try {
+            return Double.parseDouble(n);
+        } catch (NumberFormatException e) {
+            System.err.println("Error de parsing: " + errMsg);
+            System.exit(1);
+        }
+        return 0;
     }
 }
